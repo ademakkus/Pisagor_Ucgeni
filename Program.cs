4cdbf8edb5d968d60484538ae84798bf8da1ab12
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-11-27 17:14:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
+namespace PisagorUcgeni
+{
+    class Program
+    {
+
+        public static int PisagorUcgeni(int ustSinir)
+        {
+            int count = 0;
+            for (int a = 1; a < ustSinir; a++)
+            {
+                for (int b = a; b < ustSinir; b++)
+                {
+                    for (int c = b; c < ustSinir; c++)
+                    {
+                        if (a*a+b*b==c*c)
+                        {
+                            WriteLine($"( {a},{b},{c} )");
+                            count++;
+                        }
+                        
+                    }
+                }
+            }
+            return count;
+
+        }
+        static void Main(string[] args)
+        {
+
+            WriteLine("Pisagor üçgeni sayısı : " + PisagorUcgeni(100));
+            ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PisagorUcgeni")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PisagorUcgeni")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fd69f7a-5054-4873-b1df-8de4ec27cb2b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
